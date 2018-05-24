eff8899df159506da27039caa168f7757a84b616
Kamigami55 <samkami@icloud.com>
2018-05-24 21:56:34 +0000

    Set web model item holder to optional

--- InventoryOS-web/app/models/item.rb
+++ InventoryOS-web/app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
-  belongs_to :holder, class_name: 'User', foreign_key: 'holder_id'
+  belongs_to :holder, class_name: 'User', foreign_key: 'holder_id', optional: true
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
 end
