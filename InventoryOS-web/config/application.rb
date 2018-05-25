3f33180c4fec9b1eb514f6c29a83d22b045c2462
Kamigami55 <samkami@icloud.com>
2018-05-25 19:52:29 +0800

    Web: Set sass as default css preprocessor

--- InventoryOS-web/config/application.rb
+++ InventoryOS-web/config/application.rb
@@ -15,5 +15,6 @@ module InventoryOSWeb
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.sass.preferred_syntax = :sass
   end
 end
