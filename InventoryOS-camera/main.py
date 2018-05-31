3240d861e4214075ce9e8f8ecd5049da3cd5554d
Kamigami55 <eason@easonchang.com>
2018-06-01 02:54:43 +0800

    Camera: Add feature Capture image and classify using Google Vision API

--- InventoryOS-camera/main.py
+++ InventoryOS-camera/main.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 '''
 InventoryOS camera main.py
@@ -7,13 +7,38 @@ InventoryOS camera main.py
 
 Main script of InventoryOS camera component
 
+Before run this script, you should set credential
+set environment variable $GOOGLE_APPLICATION_CREDENTIALS
+to the path of your credential.json downloaded from
+google cloud
 '''
+
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 from enum import Enum
- 
+
+
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'captured.png')
+
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = \
+    '/home/eason/projects/InventoryOS/\
+InventoryOS-camera/google-vision-credential.json'
+
+
 class ModeType(Enum):
     BORROW = 1
     RETURN = 2
@@ -31,6 +56,21 @@ class CameraLayout(BoxLayout):
         camera = self.ids['camera']
         camera.export_to_png("captured.png")
         print("Captured")
+
+        # Start performing object detection using Google Vision API
+        # Loads the image into memory
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+
+        image = types.Image(content=content)
+
+        # Performs label detection on the image file
+        response = visionClient.label_detection(image=image)
+        labels = response.label_annotations
+
+        print('Labels:')
+        for label in labels:
+            print(label.description)
 
     def switchMode(self):
         '''
@@ -55,6 +95,9 @@ class CameraApp(App):
 
 if __name__ == '__main__':
     try:
+        # Instantiates a client for google vision API
+        visionClient = vision.ImageAnnotatorClient()
+
         CameraApp().run()
     except KeyboardInterrupt:
         print("Exit")
